bacdf4e29547967a6a82e293aca00d42023c0195
robertm <robert.munro@thetrainline.com>
2017-11-21 09:36:40 +0000

    add test libraries

--- net/src/test/kotlin/uk/co/sentinelweb/bitwatcher/net/ExchangeProviderTest.kt
+++ net/src/test/kotlin/uk/co/sentinelweb/bitwatcher/net/ExchangeProviderTest.kt
@@ -1,21 +1,30 @@
 package uk.co.sentinelweb.bitwatcher.net
 
+import com.nhaarman.mockito_kotlin.mock
 import org.junit.Before
 import org.junit.Test
 import org.knowm.xchange.currency.CurrencyPair
 
 class ExchangeProviderTest {
+
     lateinit var sut: ExchangeProvider
+
+//    private val key: String = mock()
+//    private val secret: String = mock()
+//    private val user: String = mock()
+
     @Before
     fun setUp() {
         val key = System.getProperty("BAL_API_KEY")
         val secret = System.getProperty("BAL_SECRET")
         val user = System.getProperty("BITSTAMP_USER")
+
         sut = ExchangeProvider(key, secret, user)
     }
 
     @Test
     fun getExchnage() {
+
         sut.exchnage.marketDataService.getTicker(CurrencyPair.BTC_USD);
 
     }
