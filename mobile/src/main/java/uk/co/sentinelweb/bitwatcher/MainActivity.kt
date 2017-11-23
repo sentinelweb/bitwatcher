25f9222085cfd2913504bc955ac0115342af680b
Robert Munro <rob@sentinelweb.co.uk>
2017-11-23 08:25:02 +0000

    fix viewpager issue and add icons

--- mobile/src/main/java/uk/co/sentinelweb/bitwatcher/MainActivity.kt
+++ mobile/src/main/java/uk/co/sentinelweb/bitwatcher/MainActivity.kt
@@ -5,15 +5,13 @@ import android.support.design.widget.BottomNavigationView
 import android.support.v4.view.PagerAdapter
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.util.SparseArray
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.activity_main.*
-import uk.co.sentinelweb.bitwatcher.pages.PagePresenter
 import uk.co.sentinelweb.bitwatcher.pages.PagerFactory
 
 class MainActivity : AppCompatActivity(), MainContract.View {
-    private lateinit var presenter: MainContract.Presenter;
+    private lateinit var presenter: MainContract.Presenter
     private val pagerFactory = PagerFactory()
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
@@ -47,7 +45,7 @@ class MainActivity : AppCompatActivity(), MainContract.View {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        presenter = MainPresenter(this);
+        presenter = MainPresenter(this)
         presenter.onCreate()// TODO use lifecycle componets
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
@@ -55,14 +53,14 @@ class MainActivity : AppCompatActivity(), MainContract.View {
             override fun instantiateItem(container: ViewGroup?, position: Int): Any? {
                 val pagePresenter = pagerFactory.createPagePresenter(container, position)
                 presenter.addPagePresenter(position, pagePresenter)
-                Log.d("MainActivity","add item ${position}")
+                Log.d("MainActivity", "add item ${position}")
                 return pagePresenter.view()
             }
 
             override fun destroyItem(container: ViewGroup?, position: Int, `object`: Any?) {
-                Log.d("MainActivity","remove item ${position}")
-                presenter.removePagePresenter(position)
-                container?.removeView(container.getChildAt(0))
+                Log.d("MainActivity", "remove item ${position}")
+                val removePagePresenter = presenter.removePagePresenter(position)
+                container?.removeView(removePagePresenter?.view())
             }
 
             override fun isViewFromObject(view: View?, `object`: Any?): Boolean {
